4cecd45cc30f58ee0783454fb8f955ef0d499758
GitHubYogendra <yogendragowdak006@gmail.com>
2022-01-17 11:01:54 +0530

    online wine shop

---
+++ files/proshop_django copy/backend/frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+function Footer() {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className="text-center py-3"><span>&#128515;</span> Thank You For Visiting Our Website <span>&#128515;</span></Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
